aad159b8b36b5ed348159bf42b166af6f7da4aa2
Sam Xu <yxu8@stuy.edu>
2016-02-04 19:18:15 -0500

    Completed 01recursion

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -4,7 +4,7 @@ public class Recursion implements hw01{
     }
 
     public Boolean helpsqrt(double current, double y){
-	return Math.abs((Math.pow(current,2)/y) - 1) < 0.0001; 
+	return Math.abs((Math.pow(current,2)/y) - 1) < 0.00001; 
     }
 
     public double sqrt(double n){
@@ -12,14 +12,14 @@ public class Recursion implements hw01{
 	    throw new IllegalArgumentException("n < 0");
 	}
 	double counter = n/2;
-	while(helpsqrt(counter, n)){
+	while(!helpsqrt(counter, n)){
 	    counter = (n/counter + counter)/2;
 	}
 	return counter;
     }
 
-    public static void main(string [] args){
+    /*public static void main(String [] args){
 	Recursion x = new Recursion();
-	System.out.println(Recursion.sqrt(20));
-    }
+	System.out.println(x.sqrt(20));
+    }*/
 }
