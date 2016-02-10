072c62f177dd6bfc0fe6815cb6560681da085bee
Sam Xu <yxu8@stuy.edu>
2016-02-09 21:06:28 -0500

    construction of the board class

--- 02NQueens/Board.java
+++ 02NQueens/Board.java
@@ -28,7 +28,20 @@ public class Board{
     }
 
     public void addQueen(int row, int col){
-	
+	if(board[row][col] == 0){
+	    board[row][col] = 1;
+	}
+	for(int i = 0; i < board.length; i++){
+	    if(board[row][i] <= 0){
+		board[row][i] -= 1;
+	    }
+	    if(board[i][col] <= 0){
+		board[i][col] -= 1;
+	    }
+	}
+	/*for(int i = 0; i < board.length - row;i++){
+	    if(board[row][col+i])
+	    }*/
     }
 
     public void removeQueen(int row, int col){
@@ -37,6 +50,7 @@ public class Board{
 
     public static void main(String [] args){
 	Board x = new Board();
+	x.addQueen(0,0);
 	x.PrintBoard();
     }
 
