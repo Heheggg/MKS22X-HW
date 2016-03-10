a03f8deef2d198391f1c53ca9fea2f2c953e1695
Sam Xu <yxu8@stuy.edu>
2016-03-09 22:46:05 -0500

    added quickSort

--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -4,14 +4,13 @@ public class Quick{
 
     private static int partition(int[]ary, int left, int right){
 	int middlenum =ary[(left+right)/2];
-	ary[(left+right)/2] = ary[right];
-	ary[right] = middlenum;
+        swap(ary,(left+right)/2,right);
 	
 	
 	int i = left;
 	int k = right-1;
 	
-	while(i <= k-1){
+	while(i < k){
 	    while(ary[i]< middlenum){
 		i++;
 	    }
@@ -19,7 +18,7 @@ public class Quick{
 		k--;
 	    }
 
-	    if(i <= (right-1)){
+	    if(i <= k){
 		int save = ary[i];
 		ary[i] = ary[k];
 		ary[k] =  save;
@@ -28,12 +27,21 @@ public class Quick{
 	    }
 	}
 
+
+
 	for(int ind = left; ind < right; ind++){
 	    if(ary[ind] > middlenum){
+		swap(ary,ind,right);
 		return ind;
 	    }
 	}
 	return right;	    
+    }
+
+    private static void swap(int [] ary, int a, int b){
+	int temp = ary[a];
+	ary[a] = ary[b];
+	ary[b] = temp;
     }
 
     
@@ -82,9 +90,34 @@ public class Quick{
 	    
     }
 
+    public static void quickSort(int [] ary){
+	quickSort(ary,0,ary.length-1);
+    }
+
+    public static void quickSort(int [] ary, int bot, int top){
+	int pivot = partition(ary,bot,top);
+	if(bot < top){
+	    if(pivot > bot){
+		quickSort(ary,bot,pivot-1);
+	    }
+	    if(pivot < top){
+		quickSort(ary,pivot+1,top);
+	    }
+	}
+    }
+
+    public static String name(){
+	return "6,Xu,Yueyang";
+    }
+
     public static void main(String [] args){
 	int [] test1 = { 2, 5, -2, 6, -3, 8, 0, -7, -9, 4 };
-	System.out.println(Quick.partition(test1,0,4));
+        quickSort(test1);
+
+	for(int i = 0; i < test1.length;i++){
+	    System.out.print(test1[i]+",");
+	}
+
 
     }
     
