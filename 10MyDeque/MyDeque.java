9d1ec8b1c6bb0dc3cd593b3fe4060af5f577954c
Sam Xu <yxu8@stuy.edu>
2016-04-03 21:02:49 -0400

    adds dequeue

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -22,30 +22,87 @@ public class MyDeque<T>{
 	    size = 1;
 	    return true;
 	}
+	if(start == 0){
+	    start = array.length-1;	    
+	}else{
+	    start = start-1;
+	}
+	array[start] = x;
+	size++;
 	return true;
     }
 
-    /*public boolean addLast(T x){
+    public boolean addLast(T x){
+	if(size >= array.length){
+	    throw new IllegalStateException();
+	}
+	if(size == 0){
+	    array[end] = x;
+	    size = 1;
+	    return true;
+	}
+	if(end == array.length-1){
+	    end = 0;	    
+	}else{
+	    end = end+1;
+	}
+	array[end] = x;
+	size++;
+	return true;
     }
 
     public T peekFirst(){
-
+	return array[start];
     }
 
     public T peekLast(){
-
+	return array[end];
     }
 
     public T pollFirst(){
-
+	if(size == 0){
+	    throw new IllegalStateException();
+	}
+	T save = array[start];
+	array[start] = null;
+	if(start == array.length-1){
+	    start = 0;
+	}else{
+	    start++;
+	}
+	return save;
     }
 
     public T pollLast(){
-
+	if(size == 0){
+	    throw new IllegalStateException();
+	}
+	T save = array[end];
+	array[end] = null;
+	if(end == 0){
+	    end = array.length-1;
+	}else{
+	    end --;
+	}
+	return save;
     }
 
     private void resize(){
-    
+	if(size == 0){
+	    throw new IllegalStateException();
+	}
+	T [] newary = (T[]) new Object[array.length*2];
+	int counter = 0;
+	do{
+	    newary[counter] = array[start];
+	    if(start == array.length-1){
+		start = 0;
+	    }else{
+		start++;
+	    }
+	}while(start!=end);
+	
+	start = 0;
+	end = size-1;  
     }
-    */
 }
