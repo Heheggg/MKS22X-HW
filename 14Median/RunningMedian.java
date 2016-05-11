4f2e90fb51dc3dbe38aa27cb62efb21115884957
Sam Xu <yxu8@stuy.edu>
2016-05-10 20:23:46 -0400

    Finish Median

---
+++ 14Median/RunningMedian.java
@@ -0,0 +1,39 @@
+@SuppressWarnings("unchecked")
+public class RunningMedian{
+    private MyHeap smaller;
+    private MyHeap bigger;
+
+    public RunningMedian(){
+	smaller = new MyHeap<Double>(true);
+	bigger = new MyHeap<Double>(false);
+    }
+
+    public double getMedian(){
+	int sizeS = smaller.getSize();
+	int sizeB = bigger.getSize();
+	if(sizeS == sizeB){
+	    return ((Double)smaller.peek() + (Double)bigger.peek())/2;
+	}else if(sizeS > sizeB){
+	    return (Double)smaller.peek();
+	}else{
+	    return (Double)bigger.peek();
+	}
+	    
+    }
+
+    public void add(double x){
+	if(x > getMedian()){
+	    bigger.add(x);
+	    if(bigger.getSize()-smaller.getSize() > 1){
+		smaller.add(bigger.delete());
+	    }
+	}else{
+	    smaller.add(x);
+	    if(smaller.getSize()-smaller.getSize() > 1){
+		bigger.add(smaller.delete());
+	    }
+	}
+    }
+
+    
+}
